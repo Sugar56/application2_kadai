e480859e4bfee0672cdb97c1231cc057fc18f106
Sugar56 <108616992+Sugar56@users.noreply.github.com>
2022-07-20 16:13:05 +0000

    [Add] アプリケーションに必要な準備

--- bookers2/config/environments/development.rb
+++ bookers2/config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "58cbf9a41e614e679b6abb675eab0eba.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
