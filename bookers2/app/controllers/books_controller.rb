fe97d46c88c95aeca32742a54b74623a3829ab5c
Sugar56 <108616992+Sugar56@users.noreply.github.com>
2022-07-26 05:33:51 +0000

    [Finish] 4-25

--- bookers2/app/controllers/books_controller.rb
+++ bookers2/app/controllers/books_controller.rb
@@ -4,22 +4,46 @@ class BooksController < ApplicationController
 
   def index
     @book = Book.new
+    @user = current_user
     @books = Book.all
   end
 
   def create
     @book = Book.new(book_params)
+    @user = current_user
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
+    @book1 = Book.new
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
     @book = Book.find(params[:id])
+    if @book.user == current_user
+      render :edit
+    else
+      redirect_to books_path
+    end
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
--- bookers2/app/models/book.rb
+++ bookers2/app/models/book.rb
@@ -1,4 +1,9 @@
 class Book < ApplicationRecord
-  has_one_attached :image
   belongs_to :user
+
+  has_one_attached :image
+
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 2, maximum: 200 }
+
 end
